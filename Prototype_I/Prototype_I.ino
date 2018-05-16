c371e7fe544c510909582449452bef598b92833c
ivovanderzee <ivovanderzee@live.nl>
2018-05-16 22:13:50 +0200

    Dit is de code voor het eerste prototype

---
+++ Prototype_I/Prototype_I.ino
@@ -0,0 +1,41 @@
+
+//Code voor het eerste prototype
+
+//Variabelen die in de code gebruikt worden
+int ldr = A0;
+int ldrValue;
+int LED = 13;
+
+void setup() {
+
+  //pin 13 word geconfigureerd voor input
+  pinMode(LED, INPUT);
+
+  //Start de console zodat hier de waarde van de LDR uitgelezen kan worden
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+//Leest de waarde van de LDR uit
+ldrValue = analogRead(LDR);
+
+//Print de waarde van de LDR in de console
+Serial.println(ldrValue)
+
+
+//IF statement dat steeds checkt wat de waarde is van de LDR
+
+//Hand op de LDR
+if(ldrValue < 10){
+
+  digitalWrite(LED, HIGH);
+
+  //Hand is niet op de LDR
+}else{
+
+  digitalWrite(LED, LOW);
+}
+
+}
